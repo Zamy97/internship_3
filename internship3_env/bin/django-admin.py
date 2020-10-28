69c0368ba0a1d419bd1c48aaea6dab88f2b43336
Aktar Zaman <aktarzaman@berkeley.edu>
2020-10-27 20:17:08 -0400

    first awesome commit

---
+++ internship3_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/zamy/Desktop/Python_Projects/excl_intrnship_projects/excl_internship_0/internship_3/internship_3/internship3_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
